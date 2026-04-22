4847c4d075503ccfb0b19f205f7d2373c0c7062a
cnd86038-ai <cnd86038@email.vccs.edu>
2026-04-22 11:28:39 -0400

    Startup and set all my function prototypes.

---
+++ StarProject/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//Function prototypes
+double getScore();
+bool isLower(double value1, double value2);
+bool isHigher(double value1, double value2);
+double calcAverage(double total, double minScore, double maxScore);
+
+
