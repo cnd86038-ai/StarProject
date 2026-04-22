49a07c316aa4b129c499e64deed6eb100b372acf
cnd86038-ai <cnd86038@email.vccs.edu>
2026-04-22 11:31:49 -0400

    Setup getting and validating the judge score between 0.0 and 10.0

--- StarProject/main.cpp
+++ StarProject/main.cpp
@@ -8,4 +8,29 @@ bool isLower(double value1, double value2);
 bool isHigher(double value1, double value2);
 double calcAverage(double total, double minScore, double maxScore);
 
+/*
+Purpose: Get and vlaidates judge's score between 0.0 and 10.0
+Preconditions: None
+Postconditions: Returns a valid score between 0.0 and 10.0
+*/
+
+double getScore()
+{
+	double score;
+	cout << "Enter the judge's score (0.0 - 10.0): ";
+	cin >> score;
+	while (score < 0.0 || score > 10.0)
+	{
+		cout << "Invalid score. Please enter a score between 0.0 and 10.0: ";
+		cin >> score;
+	}
+	return score;
+}
+
+/*
+Purpose: Determines if value1 is <= value2
+Preconditions: value1 and value2 are valid scores
+Postconditions: Returns true if value1 is <= value2, otherwise returns false
+*/
+
 
